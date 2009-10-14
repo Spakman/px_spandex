84a1a306195b2313c2af2d9a991148370ab1f748
Mark Somerville <mark@scottishclimbs.com>
2009-10-14 14:34:23 +0100

    Added a List class for handling circular lists in Honcho talking apps.

---
+++ lib/list.rb
@@ -0,0 +1,42 @@
+# Copyright (C) 2009 Mark Somerville <mark@scottishclimbs.com>
+# Released under the General Public License (GPL) version 3.
+# See COPYING
+
+class List
+  def initialize(items)
+    @items = items
+    @selected_index = 0
+  end
+
+  def selected
+    @items[@selected_index]
+  end
+
+  def select_next
+    if @selected_index < @items.length - 1
+      @selected_index += 1
+    else
+      @selected_index = 0
+    end
+    @items[@selected_index]
+  end
+
+  def select_previous
+    if @selected_index > 0
+      @selected_index -= 1
+    else
+      @selected_index = @items.length - 1
+    end
+    @items[@selected_index]
+  end
+
+  def to_s
+    list = "<list>\n"
+    @items.each do |item|
+      list << "  <item"
+      list << ' selected="yes"' if item == @items[@selected_index]
+      list << ">#{item}</item>\n"
+    end
+    list << "</list>\n"
+  end
+end
---
+++ test/list_test.rb
@@ -0,0 +1,48 @@
+require "rubygems"
+require "test/unit"
+require "#{File.dirname(__FILE__)}/../lib/list"
+
+class ListTest < Test::Unit::TestCase
+  def setup
+    items = %w( Artists Genres Playlists )
+    @list = List.new items
+  end
+
+  def test_selected
+    assert_equal "Artists", @list.selected
+  end
+
+  def test_select_next
+    assert_equal "Genres", @list.select_next
+    assert_equal "Genres", @list.selected
+  end
+
+  def test_select_next_last_item_wraps_around
+    @list.instance_eval "@selected_index = 2"
+    assert_equal "Artists", @list.select_next
+    assert_equal "Artists", @list.selected
+  end
+
+  def test_select_previous
+    @list.instance_eval "@selected_index = 2"
+    assert_equal "Genres", @list.select_previous
+    assert_equal "Genres", @list.selected
+  end
+
+  def test_select_previous_first_item_wraps_around
+    assert_equal "Playlists", @list.select_previous
+    assert_equal "Playlists", @list.selected
+  end
+
+  def test_to_s
+    @list.instance_eval "@selected_index = 1"
+    expected = <<-LIST
+<list>
+  <item>Artists</item>
+  <item selected="yes">Genres</item>
+  <item>Playlists</item>
+</list>
+LIST
+    assert_equal expected, @list.to_s
+  end
+end
