2d4375075f080cf4ea32be926588a796d79ea5b1
Mark Somerville <mark@scottishclimbs.com>
2009-10-23 15:39:36 +0100

    List#to_s now only generates the number of list items to be displayed.
    It's a shame that this logic has to live here (previously Rembrandt) was
    able to take a list of any length and display it based on what was
    selected). However, the new way of displaying lists (which is the way
    the current iPods do it) requires that either Rembrandt or the List
    class keeps some sort of display state around. The List seems like the
    best place.

--- lib/list.rb
+++ lib/list.rb
@@ -4,9 +4,12 @@
 
 # A class for handling circular lists for use in Honcho talking applications.
 class List
+  NUMBER_OF_LIST_ITEMS_TO_DISPLAY = 5
+
   def initialize(items)
     @items = items
     @selected_index = 0
+    @starting_item = 0
   end
 
   def selected
@@ -15,9 +18,10 @@ class List
 
   def select_next
     if @selected_index < @items.length - 1
+      if @selected_index >= @starting_item + NUMBER_OF_LIST_ITEMS_TO_DISPLAY - 1
+        @starting_item += 1
+      end
       @selected_index += 1
-    else
-      @selected_index = 0
     end
     @items[@selected_index]
   end
@@ -25,15 +29,17 @@ class List
   def select_previous
     if @selected_index > 0
       @selected_index -= 1
-    else
-      @selected_index = @items.length - 1
+      if @starting_item > 0 and @starting_item == @selected_index + 1
+        @starting_item -= 1
+      end
     end
     @items[@selected_index]
   end
 
   def to_s
+#   puts "starting at #{@starting_item} (#{@items[@starting_item]})   and selected #{@selected_index} (#{@items[@selected_index]})"
     list = "<list>\n"
-    @items.each do |item|
+    @items[@starting_item...@starting_item+NUMBER_OF_LIST_ITEMS_TO_DISPLAY].each do |item|
       list << "  <item"
       list << ' selected="yes"' if item == @items[@selected_index]
       list << ">#{item}</item>\n"
--- test/list_test.rb
+++ test/list_test.rb
@@ -2,25 +2,58 @@ require "rubygems"
 require "test/unit"
 require_relative "../lib/list"
 
+class List
+  attr_reader :starting_item
+  remove_const :NUMBER_OF_LIST_ITEMS_TO_DISPLAY
+  NUMBER_OF_LIST_ITEMS_TO_DISPLAY = 3
+end
+
 class ListTest < Test::Unit::TestCase
   def setup
-    items = %w( Artists Genres Playlists )
+    items = %w( Artists Genres Popular Latest Never Playlists )
     @list = List.new items
   end
 
   def test_selected
     assert_equal "Artists", @list.selected
+    assert_equal 0, @list.starting_item
   end
 
   def test_select_next
     assert_equal "Genres", @list.select_next
     assert_equal "Genres", @list.selected
+    assert_equal 0, @list.starting_item
   end
 
-  def test_select_next_last_item_wraps_around
+  def test_select_next_starting_2_selected_2
+    @list.instance_eval "@starting_item = 2"
     @list.instance_eval "@selected_index = 2"
-    assert_equal "Artists", @list.select_next
-    assert_equal "Artists", @list.selected
+    assert_equal "Latest", @list.select_next
+    assert_equal "Latest", @list.selected
+    assert_equal 2, @list.starting_item
+  end
+
+  def test_select_next_incrementing_starting_index
+    @list.instance_eval "@selected_index = 2"
+    assert_equal "Latest", @list.select_next
+    assert_equal "Latest", @list.selected
+    assert_equal 1, @list.starting_item
+
+    assert_equal "Never", @list.select_next
+    assert_equal "Never", @list.selected
+    assert_equal 2, @list.starting_item
+
+    assert_equal "Playlists", @list.select_next
+    assert_equal "Playlists", @list.selected
+    assert_equal 3, @list.starting_item
+  end
+
+  def test_select_next_last_item_does_not_change
+    @list.instance_eval "@starting_item = 3"
+    @list.instance_eval "@selected_index = 5"
+    assert_equal "Playlists", @list.select_next
+    assert_equal "Playlists", @list.selected
+    assert_equal 3, @list.starting_item
   end
 
   def test_select_previous
@@ -29,18 +62,67 @@ class ListTest < Test::Unit::TestCase
     assert_equal "Genres", @list.selected
   end
 
-  def test_select_previous_first_item_wraps_around
-    assert_equal "Playlists", @list.select_previous
-    assert_equal "Playlists", @list.selected
+  def test_select_previous_first_item_does_not_change
+    assert_equal "Artists", @list.select_previous
+    assert_equal "Artists", @list.selected
   end
 
-  def test_to_s
+  def test_select_previous_starting_2_selected_4
+    @list.instance_eval "@starting_item = 2"
+    @list.instance_eval "@selected_index = 4"
+    assert_equal "Latest", @list.select_previous
+    assert_equal "Latest", @list.selected
+    assert_equal 2, @list.starting_item
+  end
+
+  def test_select_previous_decrementing_starting_index
+    @list.instance_eval "@starting_item = 3"
+    @list.instance_eval "@selected_index = 5"
+    assert_equal "Never", @list.select_previous
+    assert_equal "Never", @list.selected
+    assert_equal 3, @list.starting_item
+
+    assert_equal "Latest", @list.select_previous
+    assert_equal "Latest", @list.selected
+    assert_equal 3, @list.starting_item
+
+    assert_equal "Popular", @list.select_previous
+    assert_equal "Popular", @list.selected
+    assert_equal 2, @list.starting_item
+
+    assert_equal "Genres", @list.select_previous
+    assert_equal "Genres", @list.selected
+    assert_equal 1, @list.starting_item
+
+    assert_equal "Artists", @list.select_previous
+    assert_equal "Artists", @list.selected
+    assert_equal 0, @list.starting_item
+
+    assert_equal "Artists", @list.select_previous
+    assert_equal "Artists", @list.selected
+    assert_equal 0, @list.starting_item
+  end
+
+  def test_to_s_starting_at_zero
     @list.instance_eval "@selected_index = 1"
     expected = <<-LIST
 <list>
   <item>Artists</item>
   <item selected="yes">Genres</item>
-  <item>Playlists</item>
+  <item>Popular</item>
+</list>
+LIST
+    assert_equal expected, @list.to_s
+  end
+
+  def test_to_s_starting_at_one
+    @list.instance_eval "@selected_index = 1"
+    @list.instance_eval "@starting_item = 1"
+    expected = <<-LIST
+<list>
+  <item selected="yes">Genres</item>
+  <item>Popular</item>
+  <item>Latest</item>
 </list>
 LIST
     assert_equal expected, @list.to_s
